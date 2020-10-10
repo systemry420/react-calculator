89a4557acc2736d8c861b0b482da6459583578ba
hsn <systemry420gmail.com>
2020-10-10 16:35:45 +0300

    style elements before getting into logic

--- src/App.js
+++ src/App.js
@@ -4,7 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-
+      <div id="display" className="display">123</div>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button  className="big-v">1</button>
+      <button>2</button>
+      <button className="big-h">3</button>
+      <button>4</button>
     </div>
   );
 }
